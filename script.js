8520721bfca7811b555404200ff6d099725560d6
ivanishcenko <104127674+ivanishcenko@users.noreply.github.com>
2022-04-21 19:12:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+const ageQuestion = prompt('Enter your age');
+const age = ageQuestion >= 18 ? 'You can buy alcohol' : 'You can not buy the alcohol, you are too young.';
+alert(age);
+console.log(age);
